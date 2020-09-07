387aa9067e8524ddaf36052ceb16d49c8ddb36c3
Osidel <osidel79@gmail.com>
2020-09-06 17:05:04 -0700

    changes text in app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          this is change for example
         </a>
       </header>
     </div>
